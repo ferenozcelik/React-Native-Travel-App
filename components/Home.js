993f215f8139288278bb2f072c0dcbcfeb1d76a4
Fatih Eren Özçelik <eren.ozcelik@std.antalya.edu.tr>
2022-07-26 12:21:56 +0300

    finished home page

--- components/Home.js
+++ components/Home.js
@@ -59,6 +59,20 @@ const Home = ({navigation}) => {
     );
   };
 
+  const renderLearnMoreItem = ({item}) => {
+    return (
+      <ImageBackground
+        source={item.image}
+        style={[
+          styles.learnMoreItem,
+          {marginLeft: item.id === 'learnMore-1' ? 20 : 0},
+        ]}
+        imageStyle={styles.learnMoreItemImage}>
+        <Text style={styles.learnMoreItemText}>{item.title}</Text>
+      </ImageBackground>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView>
@@ -104,6 +118,20 @@ const Home = ({navigation}) => {
             <FlatList
               data={activitiesData}
               renderItem={renderActivityItem}
+              keyExtractor={item => item.id}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+            />
+          </View>
+        </View>
+
+        {/* Learn More - touchable eklenecek*/}
+        <View style={styles.learnMoreWrapper}>
+          <Text style={styles.learnMoreTitle}>Learn More</Text>
+          <View style={styles.learnMoreItemsWrapper}>
+            <FlatList
+              data={learnMoreData}
+              renderItem={renderLearnMoreItem}
               keyExtractor={item => item.id}
               horizontal
               showsHorizontalScrollIndicator={false}
@@ -192,7 +220,7 @@ const styles = StyleSheet.create({
     color: colors.black,
   },
   activitiesItemsWrapper: {
-    paddingVertical: 10,
+    paddingVertical: 20,
   },
   activityItemWrapper: {
     justifyContent: 'flex-end',
@@ -207,6 +235,34 @@ const styles = StyleSheet.create({
     fontFamily: 'Lato-Bold',
     fontSize: 14,
     color: colors.gray,
+  },
+  learnMoreWrapper: {
+    marginTop: 10,
+  },
+  learnMoreTitle: {
+    marginHorizontal: 20,
+    fontFamily: 'Lato-Bold',
+    fontSize: 24,
+    color: colors.black,
+  },
+  learnMoreItemsWrapper: {
+    paddingVertical: 20,
+  },
+  learnMoreItem: {
+    width: 170,
+    height: 180,
+    justifyContent: 'flex-end',
+    marginRight: 20,
+  },
+  learnMoreItemImage: {
+    borderRadius: 20,
+  },
+  learnMoreItemText: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 18,
+    color: colors.white,
+    marginBottom: 20,
+    marginLeft: 10,
   },
 });
 
